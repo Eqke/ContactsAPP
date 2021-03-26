76c8499a9da04a3da70a8314402a97f2fe9004fa
Eqke <KrazyToinik@gmail.com>
2021-03-26 13:15:14 +0700

    Update newton.json, fixed tests.

--- src/ContactsApp/ContactsApp.UnitTests/ProjectManagerTests.cs
+++ src/ContactsApp/ContactsApp.UnitTests/ProjectManagerTests.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using System.Text;
 using NUnit.Framework;
 using ContactsApp;
+using Newtonsoft.Json;
 
 namespace ContactsApp.UnitTests
 {
     [TestFixture]
     public class ProjectManagerTests
     {
-        
+        public string Path
+        {
+            get
+            {
+                var location = Assembly.GetExecutingAssembly().Location;
+                location = location.Replace("\\ContactsApp.UnitTests.dll", "\\TestData\\");
+                return location;
+            }
+        }
+
         [TestCase(Description = "Positive project manager load test", TestName = "Load corrected file")]
         public void ProjectManager_LoadCorrectData_FileLoadCorrected()
         {
@@ -24,7 +35,8 @@ namespace ContactsApp.UnitTests
             expectedProject.Contacts.Add(contact);
 
             // Act
-            var actualProject = ProjectManager.LoadFromFile("TestData\\", "CorrectedProjectFile.json");
+            var actualProject = ProjectManager.LoadFromFile(Path
+                , "CorrectedProjectFile.json");
             
             // Assert
             Assert.AreEqual(expectedProject.Contacts.Count, actualProject.Contacts.Count);
@@ -37,20 +49,13 @@ namespace ContactsApp.UnitTests
         [TestCase(Description = "", TestName = "Load incorrected file")]
         public void ProjectManager_LoadBrokenData_FileLoadIncorrected()
         {
-            // Setup
-            var expectedProject = new Project();
-            var contact = new Contact("Козлюк", "Василий", new DateTime(2000, 2, 25),
-                "eqkez0r@mail.ru", new PhoneNumber("79234364565"), "eqkez0r");
-            expectedProject.Contacts.Add(contact);
-            contact = new Contact("Семенов", "Алексей", new DateTime(2000, 3, 20),
-                "sem@mail.ru", new PhoneNumber("79534364565"), "sem");
-            expectedProject.Contacts.Add(contact);
-
-            // Act
-            var actualProject = ProjectManager.LoadFromFile("TestData\\", "BrokenProjectFile.json");
-
+            
             // Assert
-            Assert.AreNotEqual(expectedProject.Contacts.Count, actualProject.Contacts.Count);
+            Assert.Throws<Newtonsoft.Json.JsonReaderException>(() =>
+                {
+                    var actualProject = ProjectManager.LoadFromFile(Path, "BrokenProjectFile.json");
+                }
+                );
         }
 
         [TestCase(Description = "", TestName = "Save correctly test")]
@@ -66,11 +71,11 @@ namespace ContactsApp.UnitTests
             savingProject.Contacts.Add(contact);
 
             // Act
-            ProjectManager.SaveToFile(savingProject,"TestData\\", "SavingProjectFile.json");
+            ProjectManager.SaveToFile(savingProject, Path, "SavingProjectFile.json");
 
             // Assert
-            var expected = File.ReadAllText("TestData\\CorrectedProjectFile.json");
-            var actual = File.ReadAllText("TestData\\SavingProjectFile.json");
+            var expected = File.ReadAllText(Path + "\\CorrectedProjectFile.json");
+            var actual = File.ReadAllText(Path + "\\SavingProjectFile.json");
             Assert.AreEqual(expected, actual);
         }
     }
